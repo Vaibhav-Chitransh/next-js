81c4de162a8a1d6ded66e3641a534c3bb12058b1
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2026-06-27 18:59:29 +0530

    custom not found page and loading.js

--- level1/src/app/page.js
+++ level1/src/app/page.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+  // promise setTimeout to show custom loading page
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  })
+
   return (
     <div>
       Home page
