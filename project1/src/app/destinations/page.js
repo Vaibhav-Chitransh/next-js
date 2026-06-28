3f7514d893ac48b67357f4468524db221d1d1425
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2026-06-28 12:24:39 +0530

    global destination page

--- project1/src/app/destinations/page.js
+++ project1/src/app/destinations/page.js
@@ -1,9 +1,23 @@
+'use client'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const Destinations = () => {
+    const destinations = ['NewYork', 'Paris', 'Tokyo'];
+    const router = useRouter();
+
   return (
-    <div>
-        Destinations page
+    <div className='text-white flex flex-col gap-4 items-center justify-center text-2xl h-full'>
+        <span>Choose your Destination</span>
+        <div>
+            {destinations.map((des, index) => {
+                return (
+                    <div key={index} className='bg-white text-black m-4 px-4 py-2 text-center rounded-lg cursor-pointer transform transition-transform duration-300 hover:scale-110' onClick={() => router.push(`/destinations/${des}`)}>
+                        {des}
+                    </div>
+                )
+            })}
+        </div>
     </div>
   )
 }
