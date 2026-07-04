adfdb6366221f321ca360df1e302008113f49ec2
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2026-07-04 18:49:34 +0530

    writing credentials jwt and session

--- project2/src/lib/auth.ts
+++ project2/src/lib/auth.ts
@@ -34,7 +34,29 @@ const authOptions : AuthOptions = {
             }
         })
     ],
-    callbacks: {},
+    callbacks: {
+        async jwt({token, user}) {
+            if(user) {
+                token.id = user.id;
+                token.name = user.name;
+                token.email = user.email;
+                token.image = user.image;
+            }
+
+            return token;
+        },
+
+        session({session, token}) {
+            if(session.user) {
+                session.user.id = token.id as string;
+                session.user.name = token.name;
+                session.user.email = token.email;
+                session.user.image = token.image as string;
+            }
+
+            return session;
+        }
+    },
     session: {},
     pages: {},
     secret: ""
